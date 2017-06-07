a23dd993aa074329bbcaa600fe14b6618cf5b105
gpdoud <gpdoud@gmail.com>
2017-06-07 12:28:54 -0400

    initial commit

---
+++ ConsoleAppUsingLibraryProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryUsedByConsoleAppProject;
+
+namespace ConsoleAppUsingLibraryProject {
+    class Program {
+
+        static void Main(string[] args) {
+
+            System.Diagnostics.Debug.WriteLine(LibraryUsedByConsoleAppProject.Copyright.Author);
+
+        }
+    }
+}
---
+++ LibraryUsedByConsoleAppProject/Copyright.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryUsedByConsoleAppProject
+{
+    public class Copyright {
+
+        public static string Author = "Greg Doud";
+
+    }
+}
